d8d619cf8c2ede5ac15d4b206fd2ad3e76ebf212
utpalmodi <101075153+utpalmodi@users.noreply.github.com>
2022-04-06 14:54:02 +1200

    updated

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,5 +1,11 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop', 'customers') }}
+with
+    src as (select * from from {{ source("jaffle_shop", "customers") }}),
+    stg as (
+        select
+            id as customer_id,
+            first_name as customer_first_name,
+            last_name as customer_last_name
+        from src
+    )
+select *
+from stg
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,13 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-
-from raw.jaffle_shop.orders
+with
+    src as (select * from {{ source("jaffle_shop", "orders") }}),
+    stg as (
+        select
+            id as order_id,
+            user_id as customer_id,
+            order_date as order_placed_at,
+            status,
+            status as order_status
+        from src
+    )
+select *
+from stg
